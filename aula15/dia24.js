2e8df185dec93a13b3effdbf15ac3f95c6de4be0
DenisSmith52 <dennisaraujorochaaraujorocha@gmail.com>
2024-05-24 14:31:59 -0500

    auladia24demaio

---
+++ aula15/dia24.js
@@ -0,0 +1,6 @@
+var nome = "Dennis"/*string pode ser tanbém  outra forma de se chamar uma cadeia*/
+var idade = 14 /*number ele representa um valores de numeros intebendendi se seja real ou inteiro*/ 
+var real = 9.8
+document.write(nome + "<br>")
+document.write(idade)
+document.write(real)
