d8fdeeac240e8a47079bc846b3cd3ed242dd1052
DenisSmith52 <dennisaraujorochaaraujorocha@gmail.com>
2024-06-12 14:37:48 -0500

    junho

---
+++ aula12junho/smit.js
@@ -0,0 +1,7 @@
+var daf = (prompt("digite um valor menor que 200"))
+if(daf >= 80){
+    alert(" o daf é verdadeiro")
+}
+else{
+     alert("o daf não é verdadeiro")
+}
---
+++ aula12junho/verdadeoufalso.js
@@ -0,0 +1,21 @@
+var tex = " esse é o resultado do primeiro teste "
+var num1 = Number(prompt("digite um  número:"))
+var num2 = Number(prompt("digite um número maior que o primeiro: "))
+ var maior = num1 > num2
+  alert(maior)
+   document.write(maior + " " + tex + "<br>")
+
+var tex = "esse é o resultado do segundo teste "
+var num3 = Number(prompt("digite um valor:"))
+var num4 = Number(prompt("digite um valor menor que o primeiro:"))
+ var menor = num3 < num4
+  alert(menor)
+   document.write(menor + " " + tex + "<br>")
+    
+var tex = "esse é o resultado do terceiro teste"
+var num5 = Number(prompt("digite um valor"))
+var num6 = Number(prompt("digite um valor maior ou igual o primeiro"))
+ var maio_igual = num5 >= num6
+  alert(maior_igual)
+   document.write(maior_igual + " " + tex + "<br>" )
+   
