0ed76d74d724c6e9e515c458ae279c1f2214da5b
DenisSmith52 <dennisaraujorochaaraujorocha@gmail.com>
2024-06-13 14:34:18 -0500

    junho13

---
+++ junhodia13/cod.js
@@ -0,0 +1,13 @@
+var idade = Number(prompt("digite sua idade:"))
+if(idade == 0 && idade < 0 ){
+    
+}
+    else if(idade > 0 ){
+        alert(" o numero é maior do que zero seu porra")
+        
+    }
+
+ else{
+     alert(" o núme que voce esculheu é igual a zero!") 
+}
+
