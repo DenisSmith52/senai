9e20335481d8455996aef93671386ab738233c0b
DenisSmith52 <dennisaraujorochaaraujorocha@gmail.com>
2024-07-11 10:31:41 -0500

    julhodia11

--- julhodia10/sombra.js
+++ julhodia10/sombra.js
@@ -5,36 +5,40 @@ while (true) {
         var nome = prompt('diga seu nome completo:')
         var idade = prompt('diga sua idade:')
         var email = prompt('diga seu e-mail:')
-        alert('parabéns cadastro comcluido com sucesso!')
         funcionario.push(nome)
+        alert('parabéns cadastro comcluido com sucesso!')
+        }
+        else if (idade < 18) {
+        alert(" desculpa mas você é menor de idade e não pode fazer o cadastro! ")
+
     }
     else if (kl == 2) {
-        funcionario.forEach(function (nome) {
+        funcionario.forEach(function(nome) {
             alert(nome)
         })
+        if(funcionario.length == 0){
+            alert('nenhum funcionario encontrado!')
+        }
     }
     else if (kl == 3) {
         var excluir = prompt(' diga o nome do funcionário')
-        var indice_do_funcionario = 1
-        funcionario.forEach(function (nome, indice) {
-            if (excluir == nome) {
-                indice_do_funcionario == indice
-            }
-        })
-        if (indice_do_funcionário !== 1) {
-            funcionario.splice(indice_do_funcionário, 1)
-            alert('funcionário excluido com sucesso!')
-        }
-        else {
-            alert('funcionario não encontrado')
+        var indice_do_funcionario = funcionario.indexOf(excluir)
 
-        }
+        if(indice_do_funcionario !== -1){
+            funcionario.splice(indice_do_funcionario,1)
+            alert('funcionario excluido com sucesso!')
+        
     }
-    else if (kl == 4) {
-        alert('fim do programa')
+    }
+    else if(kl == 4){
+        alert('fim do programa!')
         break
     }
-}
+    }
+     
+    
+
+
 
 
 
