23b6d4c84ec15b13049d04b5076a50ac48e71764
DenisSmith52 <dennisaraujorochaaraujorocha@gmail.com>
2024-08-07 14:54:18 -0500

    primeiraaulasobredom

---
+++ agostodia7/deca.js
@@ -0,0 +1,11 @@
+var deca = document.querySelector("#deca")
+
+deca.innerHTML = 'altererei o texto no js'
+
+deca.style.color = 'turquoise'
+
+var sumor = document.querySelector("#sumor")
+
+sumor.innerHTML = 'amogenim'
+
+sumor.style.color = 'lime'
